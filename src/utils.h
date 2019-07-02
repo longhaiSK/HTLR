2e7649c8ae7e08e20cbd30bdbc85bca8c12c1f4a
wxlxy95 <liuxinyu9090@gmail.com>
2019-07-02 00:38:52 -0600

    add row_sum

--- src/utils.h
+++ src/utils.h
@@ -5,6 +5,7 @@
 
 arma::mat cpvec(const arma::mat &A);
 arma::vec col_sum(const arma::mat &A);
+arma::vec row_sum(const arma::mat &A);
 double log_sum_exp(const arma::vec &a);
 arma::vec log_sum_exp(const arma::mat &A);
 arma::mat find_normlv(const arma::mat &lv);
