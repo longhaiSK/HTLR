384ab8bff73a6f8be35a59a937f3bbbc58801d33
wxlxy95 <liuxinyu9090@gmail.com>
2019-08-09 01:17:38 -0600

    add docs for gendata_MLR

--- R/gendata.r
+++ R/gendata.r
@@ -1,27 +1,44 @@
-
-htlr_gendata <- function (n, p, NC = 3, nu = 2, w = 1, X = NULL, betas = NULL )
+#' Generate simulated data with multinomial logistic regression model
+#' 
+#' This function generates the response variables \code{y} given 
+#' optional supplied \code{X} using a multinomial logistic regression model.
+#' 
+#' @param n Number of observations.
+#' @param p Number of features.
+#' @param NC Number of classes for response variables.
+#' @param nu,w The regression coefficients are generated with t prior with df = \code{nu}, scale = \code{sqrt(w)}.
+#' @param X The design matrix; will be generated from standard normal distribution if not supplied.
+#' 
+#' @return A list contains design matrix \code{X}, response variables \code{y}, and regression coefficients \code{deltas}.      
+#'
+#' @export
+#' 
+#' @examples
+#' dat <- htlr_gendata_MLR(n = 100, p = 10)
+#' ggplot2::qplot(dat$y, bins = 6)
+#' corrplot::corrplot(cor(dat$X))
+#' 
+htlr_gendata_MLR <- function(n, p, NC = 3, nu = 2, w = 1, X = NULL, betas = NULL)
 {
-  if (is.null (X))
+  if (is.null(X))
+	  X <- matrix(rnorm (n * p), n, p)
+
+  if (is.null(betas))
   {
-	X <- matrix (rnorm (n * p), n, p)
+    sigmasbt <- 1 / rgamma(p, nu / 2, nu / 2) * w
+    betas <- replicate(NC, rnorm(p + 1)) * c(0, sqrt(sigmasbt))
   }
+  
+  deltas <- betas[, -1, drop = FALSE] - betas[, 1]  
+  lv <- cbind(1, X) %*% betas
+  probs <- exp(lv - as.vector(log_sum_exp(lv)))
 
-  if (is.null (betas))
-  {
-    sigmasbt <- 1/rgamma (p, nu/2, nu/2) * w
-    betas <-  replicate (NC, rnorm (p+1)) * c(0,sqrt (sigmasbt))
-  }
-  deltas <- betas[,-1, drop = FALSE] - betas[,1]  
-  lv <- cbind(1,X) %*% betas
-  probs <- exp (lv - as.vector(log_sum_exp(lv)))
+  y <- apply(probs, 1, function(prob){sample(1:NC, 1, TRUE, prob)})
 
-  y <- apply (probs, 1, function (prob) sample (1:NC, 1, TRUE, prob))
-
-  list (X = X, y = y, deltas = deltas)
-
+  list("X" = X, "y" = y, "deltas" = deltas)
 }
 
-gendata_fam <- function (n, muj, A, sd_g = 0, stdx = FALSE)
+htlr_gendata_FAM <- function (n, muj, A, sd_g = 0, stdx = FALSE)
 {
   p <- nrow (muj)
   C <- ncol (muj)
