cb892979c99952c1f8d399b0f042065fae19c1eb
wxlxy95 <liuxinyu9090@gmail.com>
2019-08-23 12:06:33 -0600

    fix and export evaluate_pred

--- R/compred.r
+++ R/compred.r
@@ -1,75 +1,98 @@
+#' Evaluate Prediction Results
+#' 
+#' This function compares the prediction results returned by a classifier with ground truth, 
+#' and finally gives a summary of the evaluation.
+#' 
+#' @param y.pred A matrix of predicted probabilities, as returned by a classifier.
+#' 
+#' @param y.true Ground truth labels vector.
+#' 
+#' @param caseid The names of test cases which we take account of. By default all test cases are used for evaluation.
+#' 
+#' @param showplot Logical; if \code{TRUE}, a summary plot will be generated.
+#' 
+#' @param ... Not used.    
+#'          
+#' @return A summary of evaluation result.
+#' 
+#' @export
+#' 
+evaluate_pred <- function(y.pred, y.true, caseid = names(y.true), showplot = TRUE, ...)
+{
+  if (is.null(caseid))
+    caseid <- 1:length(y.true)
+  
+  C <- ncol(y.pred)
+  values_pred <- apply(y.pred, 1, which.max)
+  
+  if (min(y.true) == 0) y.true <- y.true + 1
+  
+  table_eval <- data.frame(caseid, y.true, y.pred, 1 * (values_pred != y.true))
+  colnames(table_eval) <- c("Case ID", "True Label", paste ("Pred. Prob", 1:C), "Wrong?")
+  
+  eval_tab_pred(table_eval, showplot = showplot)
+}
 
 probs_attrue_bplr <- function (probs_pred, y)
 {
   tp <- rep(0, nrow(probs_pred))
-  for(i in 1:nrow(probs_pred)) tp[i] <- probs_pred[i,y[i]]
+  for(i in 1:nrow(probs_pred)) 
+    tp[i] <- probs_pred[i, y[i]]
 
   tp
 }
 
-evaluate_pred <- function (probs_pred, y, caseid = names (y), showplot = TRUE, method = "Prediction")
-{
-    if (is.null (caseid)) caseid <- 1:length (y)
-
-    C <- ncol (probs_pred)
-    values_pred <- apply (probs_pred, 1, which.max)
-    
-    table_eval <- data.frame (caseid, y, probs_pred, 1 * (values_pred != y)) 
-    colnames (table_eval) <- c("Case ID", "True Label", paste ("Pred. Prob", 1:C), "Wrong?")
-    
-    eval_tab_pred (table_eval, showplot = showplot, method = method)
-}
-
-
 eval_tab_pred <- function (table_eval, showplot = TRUE, method = "Prediction", ...)
 {
-   if (is.character (table_eval)) 
-   {
-     table_eval <- as.matrix (utils::read.table (table_eval))
-   }
-   
-   C <- ncol (table_eval) - 3
-   colnames (table_eval) <- c("Case ID", "True Label", paste ("Pred. Prob", 1:C), "Wrong?")
-   
-   probs_pred <- table_eval [, 2+(1:C)]
-   y <- table_eval[,2]
-   probs_at_truelabels <- probs_attrue_bplr (probs_pred, y)
-   which.wrong <- which (table_eval[,C+3] == 1)
-   n <- nrow (table_eval)
-   
-   amlp <- - mean (log (probs_at_truelabels))
-   no_errors <- sum (table_eval[, C+3])
-   er <- no_errors/n
-   
-   yl <- y; if (C == 2) yl[y==2] <- 3
-   
-   plotargs <- list (...)
-   if (is.null (plotargs$ylab)) 
-       plotargs$ylab <- "Predictive Probability at True Label"
-   if (is.null (plotargs$xlab)) plotargs$xlab <- "Case Index"
-   if (is.null (plotargs$ylim)) plotargs$ylim <- c(0,1)
-   if (is.null (plotargs$pch)) plotargs$pch <- yl   
-   if (is.null (plotargs$col)) plotargs$col <- 1+table_eval[, C+3]   
-   if (showplot)
-   {
-       plotargs$x <- probs_at_truelabels
-       do.call (plot, plotargs)   
-
-       if (C == 2) abline (h = 0.5)
-       abline (h = 0.1,lty = 2)
-
-       title (main = sprintf ("%s: AMLP = %5.3f, Error Rate = %4.2f%% (%d/%d)", 
-                             method,amlp, er*100, no_errors, n), 
-              cex = 0.8, line = 0.5) 
-   
-      # if (no_errors > 0) {   
-   	#	    text (which.wrong, probs_at_truelabels[which.wrong], labels = which.wrong,
-     #             srt = 90, adj = - 0.4, cex = 0.9, col = "red")
-           
-      # }
-   }
-   list (probs_at_truelabels = probs_at_truelabels, table_eval = table_eval,  
-         amlp = amlp, er = er, which.wrong = which.wrong )
+  if (is.character (table_eval)) 
+  {
+    table_eval <- as.matrix (read.table (table_eval))
+  }
+  
+  C <- ncol (table_eval) - 3
+  colnames (table_eval) <- c("Case ID", "True Label", paste ("Pred. Prob", 1:C), "Wrong?")
+  
+  probs_pred <- table_eval [, 2+(1:C)]
+  y <- table_eval[,2]
+  probs_at_truelabels <- probs_attrue_bplr (probs_pred, y)
+  which.wrong <- which (table_eval[,C+3] == 1)
+  n <- nrow (table_eval)
+  
+  amlp <- - mean (log (probs_at_truelabels))
+  no_errors <- sum (table_eval[, C+3])
+  er <- no_errors/n
+  
+  yl <- y; if (C == 2) yl[y==2] <- 3
+  
+  plotargs <- list (...)
+  if (is.null (plotargs$ylab)) 
+    plotargs$ylab <- "Predictive Prob at True Label"
+  if (is.null (plotargs$xlab)) plotargs$xlab <- "Case Index"
+  if (is.null (plotargs$ylim)) plotargs$ylim <- c(0,1)
+  if (is.null (plotargs$pch)) plotargs$pch <- yl   
+  if (is.null (plotargs$col)) plotargs$col <- 1+table_eval[, C+3]   
+  if (showplot)
+  {
+    plotargs$x <- probs_at_truelabels
+    do.call (plot, plotargs)   
+    
+    if (C == 2) abline (h = 0.5)
+    abline (h = 0.1,lty = 2)
+    
+    title (main = sprintf ("%s: AMLP = %5.3f, Error Rate = %4.2f%% (%d/%d)", 
+                           method,amlp, er*100, no_errors, n), 
+           cex = 0.8, line = 0.5) 
+    
+    # if (no_errors > 0) {   
+    #   text (which.wrong, probs_at_truelabels[which.wrong], labels = which.wrong,
+    #         srt = 90, adj = - 0.4, cex = 0.9, col = "red")
+    # }
+  }
+  list ("prob_at_truelabels" = probs_at_truelabels, 
+        "table_eval" = table_eval,  
+        "amlp" = amlp, 
+        "err_rate" = er, 
+        "which.wrong" = which.wrong)
 }
 
 # plot_features <- function (X, features, predfile = NULL, colcases = NULL, ...)
