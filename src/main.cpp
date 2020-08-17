bf22c01efdb30d3cb15c6fb5857c08d1d964179f
wxlxy95 <liuxinyu9090@gmail.com>
2020-08-17 18:37:10 -0400

    add option to keep warmup history

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,21 @@
 
 // [[Rcpp::export]]
 Rcpp::List htlr_fit_helper(
-  int p, int K, int n,
-  arma::mat &X, arma::mat &ymat, arma::uvec &ybase,
-  std::string ptype, double alpha, double s, double eta,
-  int iters_rmc, int iters_h, int thin, 
-  int leap_L, int leap_L_h, double leap_step,
-  double hmc_sgmcut, arma::mat &deltas, 
-  arma::vec &sigmasbt, int silence, bool legacy)
+    int p, int K, int n,
+    arma::mat &X, arma::mat &ymat, arma::uvec &ybase,
+    std::string ptype, double alpha, double s, double eta,
+    int iters_rmc, int iters_h, int thin,
+    int leap_L, int leap_L_h, double leap_step,
+    double hmc_sgmcut, arma::mat &deltas, arma::vec &sigmasbt,
+    bool keep_warmup_hist, int silence, bool legacy)
 {
   auto f = Fit(
-    p, K, n, X, ymat, ybase,
-    ptype, alpha, s, eta,
-    iters_rmc, iters_h, thin, 
-    leap_L, leap_L_h, leap_step, hmc_sgmcut, 
-    deltas, sigmasbt, silence, legacy);
-
+      p, K, n, X, ymat, ybase,
+      ptype, alpha, s, eta,
+      iters_rmc, iters_h, thin,
+      leap_L, leap_L_h, leap_step,
+      hmc_sgmcut, deltas, sigmasbt,
+      keep_warmup_hist, silence, legacy);
   f.StartSampling();
-  return f.OutputR(); 
+  return f.OutputR();
 }
