f8cff25dfe7bba8837a09be077df6a9dfb89407a
wxlxy95 <liuxinyu9090@gmail.com>
2020-08-21 17:34:23 -0400

    change name rank to order

--- R/rank.r
+++ R/rank.r
@@ -1,28 +1,28 @@
-#' Rank features by Kruskal-Wallis test
+#' Order features by Kruskal-Wallis test
 #'
-#' This function ranks all features in terms of Kruskal-Wallis test p-value.
+#' This function orders all features in terms of Kruskal-Wallis test p-value.
 #' 
 #' @param X Input matrix, of dimension \code{nobs} by \code{nvars}; each row is an observation vector.
 #' 
 #' @param y Vector of response variables.
 #' 
-#' @return Rank of all features of length \code{nvars}.
+#' @return Order of all features of length \code{nvars}.
 #' 
 #' @export
 #'
 #' @examples 
 #' data("diabetes392")
-#' rank_kruskal(diabetes392$X, diabetes392$y)
+#' order_kruskal(diabetes392$X, diabetes392$y)
 #' 
-rank_kruskal <- function(X, y)
+order_kruskal <- function(X, y)
 {
   pv_kt <- function(x, g) { kruskal.test(x, g)$p.value }
   order(apply(X, 2, pv_kt, g = y))
 }
 
-#' Plain rank function
+#' Plain order function
 #'
-#' A placeholder rank function that returns the original order of given features.
+#' A placeholder order function that returns the original order of given features.
 #' 
 #' @param X Input matrix, of dimension \code{nobs} by \code{nvars}; each row is an observation vector.
 #' 
@@ -34,27 +34,27 @@ rank_kruskal <- function(X, y)
 #' 
 #' @keywords internal
 #' 
-rank_plain <- function(X, y) { 1L:ncol(X) }
+order_plain <- function(X, y) { 1L:ncol(X) }
 
-#' Rank features by F-statistic
+#' Order features by F-statistic
 #'
-#' This function ranks all features in terms of ANOVA F-statistic.
+#' This function orders all features in terms of ANOVA F-statistic.
 #' 
 #' @param X Input matrix, of dimension \code{nobs} by \code{nvars}; each row is an observation vector.
 #' 
 #' @param y Vector of response variables.
 #' 
-#' @return Rank of all features of length \code{nvars}.
+#' @return Order of all features of length \code{nvars}.
 #' 
 #' @export
 #'
 #' @examples 
 #' data("diabetes392")
-#' rank_ftest(diabetes392$X, diabetes392$y)
+#' order_ftest(diabetes392$X, diabetes392$y)
 #' 
-rank_ftest <- function(X, y) { order(panova(X, y)) }
+order_ftest <- function(X, y) { order(panova(X, y)) }
 
-# Helper function for rank_ftest() 
+# Helper function for order_ftest() 
 panova <- function(X, y)
 {
   n <- length(y)
