fa42688abf377692fbc705d7feb75e405e5cc4b8
wxlxy95 <liuxinyu9090@gmail.com>
2021-05-05 16:07:10 -0400

    update install script

--- inst/install_old_htlr.sh
+++ inst/install_old_htlr.sh
@@ -1,9 +1,7 @@
 git clone https://github.com/cran/rda.git
 R CMD INSTALL --no-multiarch --with-keep.source rda
-rm -rd rda
 git clone https://github.com/longhaiSK/HTLR.git
 cd HTLR
 git checkout legacy
 cd ..
 R CMD INSTALL --no-multiarch --with-keep.source HTLR
-rm -rf HTLR
